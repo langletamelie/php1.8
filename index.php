1410ddb14306cf233d21c83bce0479d356bfa1ee
langletamelie <amelie.langlet@hotmail.com>
2018-07-25 19:21:57 +0200

    php1.8

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>PHP 1.8</title>
+</head>
+<body>
+  <p>
+    <?php
+    $number1 = 3+4;
+    echo $number1;
+    ?>
+  </p>
+  <p>
+    <?php
+    $number2 = 5*20;
+    echo $number2;
+    ?>
+  </p>
+  <p>
+    <?php
+    $number3 = 45/5;
+    echo $number3;
+    ?>
+  </p>
+</body>
+</html>
